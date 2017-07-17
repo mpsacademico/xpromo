51e5b3aba6d0d7a574b8cef852137e5c0beb9626
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-07-17 20:45:40 -0300

    Desenvolvimento da classe para modelagem do administrador do sistema

---
+++ src/br/com/twtm/xpromo/Administrador.java
@@ -0,0 +1,10 @@
+package br.com.twtm.xpromo;
+
+import java.util.Date;
+
+public class Administrador extends PessoaFisica {
+    
+    private Date dtCadastro;
+    private PessoaFisica cadastrante;
+    
+}
