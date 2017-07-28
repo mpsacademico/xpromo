e920c5b81cc6aaa2acde734b76a38b043206176a
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-07-28 19:18:22 -0300

    Adição de estrutura de dados para cadastro de mercadorias

---
+++ src/br/com/twtm/estruturas/Mercadorias.java
@@ -0,0 +1,31 @@
+package br.com.twtm.estruturas;
+
+import br.com.twtm.xpromo.Denuncia;
+import br.com.twtm.xpromo.Pessoa;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Mercadorias {
+
+    protected static int num = 10;
+    private static Queue<Pessoa> denunciados = new LinkedList();
+
+    public static void cadastrar(Pessoa p) {
+        if (!denunciados.contains(p)) {
+            denunciados.add(p);
+        }
+    }
+
+    public static Pessoa exibirPrimeira() {
+        return denunciados.element();
+    }
+
+    public static int tamanho() {
+        return denunciados.size();
+    }
+
+    public static void sair() {
+        denunciados.poll();
+    }
+
+}
